cb967ba9c463fdc2393fba52b7c9b1e1fad8bcdc
Amax Zhong <amax.zhong@outlook.com>
2026-02-04 17:31:06 +0800

    add message in details

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time': datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
-        'hostname': socket.gethostname()
+        'hostname': socket.gethostname(), 
+        'message': "Good job, man!"
     })
 
 @app.route('/api/v1/healthy')
